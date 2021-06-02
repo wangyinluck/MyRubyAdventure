d6e866f1348cecea7b34f3a1c0a079158d6f9819
gouzhun <gouzhun@libii.com>
2021-06-02 20:18:25 +0800

    add codes

--- Assets/TEST.MONSTER.cs
+++ Assets/TEST.MONSTER.cs
@@ -7,7 +7,7 @@ public class TEST.MONSTER : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("I'm a boy");
     }
 
     // Update is called once per frame
